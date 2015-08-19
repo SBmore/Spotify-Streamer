ded768eee72e341420fccd414b64e5eb49eed171
SBmore <s.blakemore87@gmail.com>
2015-08-19 22:08:13 +0100

    Worked on the DialogFragment to get the layout before adding the functionality

--- app/src/main/java/app/com/example/android/spotifystreamer/NowPlayingDialogFragment.java
+++ app/src/main/java/app/com/example/android/spotifystreamer/NowPlayingDialogFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
 * Created by Steven on 18/08/2015.
@@ -35,24 +38,15 @@ public class NowPlayingDialogFragment extends DialogFragment {
         mNum = getArguments().getInt("num");
 
         // Pick a style based on the num.
-        int style = DialogFragment.STYLE_NORMAL, theme = 0;
-        switch ((mNum-1)%6) {
-            case 1: style = DialogFragment.STYLE_NO_TITLE; break;
-            case 2: style = DialogFragment.STYLE_NO_FRAME; break;
-            case 3: style = DialogFragment.STYLE_NO_INPUT; break;
-            case 4: style = DialogFragment.STYLE_NORMAL; break;
-            case 5: style = DialogFragment.STYLE_NORMAL; break;
-            case 6: style = DialogFragment.STYLE_NO_TITLE; break;
-            case 7: style = DialogFragment.STYLE_NO_FRAME; break;
-            case 8: style = DialogFragment.STYLE_NORMAL; break;
-        }
-        switch ((mNum-1)%6) {
-            case 4: theme = android.R.style.Theme_Holo; break;
-            case 5: theme = android.R.style.Theme_Holo_Light_Dialog; break;
-            case 6: theme = android.R.style.Theme_Holo_Light; break;
-            case 7: theme = android.R.style.Theme_Holo_Light_Panel; break;
-            case 8: theme = android.R.style.Theme_Holo_Light; break;
-        }
+        int style = DialogFragment.STYLE_NO_TITLE;
+
+        int theme;
+
+//        theme = android.R.style.Theme_Holo;
+        theme = android.R.style.Theme_Holo_Light_Dialog;
+//        theme = android.R.style.Theme_Holo_Light;
+//        theme = android.R.style.Theme_Holo_Light_Panel;
+//        theme = android.R.style.Theme_Holo_Light;
         setStyle(style, theme);
     }
 
@@ -72,6 +66,10 @@ public class NowPlayingDialogFragment extends DialogFragment {
 //                ((FragmentDialog)getActivity()).showDialog();
 //            }
 //        });
+
+        ImageView thumbnailView = (ImageView) v.findViewById(R.id.album_artwork_imageview);
+        thumbnailView.setVisibility(View.VISIBLE);
+        Picasso.with(getActivity()).load("http://www.michaelbuble.com/sites/g/files/g2000002856/f/styles/album_thumbnail/public/201304/mb.jpg?itok=hbxhuTQA").into(thumbnailView);
 
         return v;
     }
